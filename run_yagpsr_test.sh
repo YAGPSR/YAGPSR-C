36b70f859b0b8bd12015bd93d69132079ebc6a36
YAGPSR <yagpsr@yagpsr.net>
2023-02-27 17:30:36 +0100

    Updated to add test complete message

--- run_yagpsr_test.sh
+++ run_yagpsr_test.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 ./yagpsr -DopplerSwing 5000 -DopplerStep 250 -DetectionThreshold 150 -CorrelationWindow 100 -SleepTime 10000 -fTestGPS -CorrelationMode FFT
+mv LatLonGPS_YAGPSR.kml TestGPS_Lat_Lon.kml
+echo "\n\nTest results: TestGPS_Lat_Lon.kml\n\n"
